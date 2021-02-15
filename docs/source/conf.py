5a7fefba346465ded2a4ed5e12b24656cf33c2f2
Bryan Thornlow <bthornlo@ucsc.edu>
2021-02-15 15:43:29 -0800

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,12 +52,10 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'default'
-html_theme_options = { 
-    'style_nav_header_background': 'white', 
-    'logo_only': True, 
-    'display_version': False,
-    'include_hidden': True
-    }
+style_nav_header_background = 'white'
+logo_only = True
+display_version = False
+include_hidden = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
